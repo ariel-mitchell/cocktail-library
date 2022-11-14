4e093f6872d40ada266810594e126c16f7cbeec0
arielmitchell <ariel.l.makowski@gmail.com>
2022-11-14 17:05:45 -0500

    startedd add-account controllers

--- src/main/java/com/liftoff/cocktaillibrary/controllers/HomeController.java
+++ src/main/java/com/liftoff/cocktaillibrary/controllers/HomeController.java
@@ -1,4 +1,28 @@
 package com.liftoff.cocktaillibrary.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
+
+    @RequestMapping("")
+    public String index(Model model) {
+        model.addAttribute("title", "Log In");
+        return "index";
+    }
+
+    @GetMapping("add-account")
+    public String displayAddAccountForm(Model model) {
+        model.addAttribute("title", "Create An Account");
+        return "user/add-account";
+    }
+
+    @PostMapping("add-account")
+    public String processAddAccountForm(Model model) {
+        return "redirect:";
+    }
 }
