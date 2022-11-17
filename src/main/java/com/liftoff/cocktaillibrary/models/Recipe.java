a852ad03a7459d96a37f02a349e0b0a36afe56bd
AaMmbm88 <103969544+AaMmbm88@users.noreply.github.com>
2022-11-17 16:04:54 -0500

    updated models

--- src/main/java/com/liftoff/cocktaillibrary/models/Recipe.java
+++ src/main/java/com/liftoff/cocktaillibrary/models/Recipe.java
@@ -2,8 +2,31 @@ package com.liftoff.cocktaillibrary.models;
 
 import java.util.List;
 
-public class Recipe {
+public class Recipe extends AbstractEntity {
 
     private List<Ingredient> ingredients;
     private List<Tag> tags;
+
+    public Recipe (){};
+
+    public Recipe (List<Ingredient> ingredients, List<Tag> tags){
+        this.ingredients = ingredients;
+        this.tags = tags;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
 }
