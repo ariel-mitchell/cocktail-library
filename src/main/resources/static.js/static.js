8c4e69b11f1015a65327c03e595c99de1743ad08
arielmitchell <ariel.l.makowski@gmail.com>
2022-11-28 17:15:02 -0500

    moved JS script into external file

---
+++ src/main/resources/static.js/static.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+       $("#ingredientType").change(function() {
+           ingredientTypeSelected();
+       });
+   });
+       function ingredientTypeSelected() {
+       let selectedType = $("#ingredientType").val();
+       let s;
+       for (i=0;i<ingredients.length;i++) {
+           if (ingredients[i].ingredientType == selectedType) {
+               s += '<option th:text="${ingredients[i].name}" th:value="${ingredients[i].id}"></option>';
+             }
+             $('#ingredient').html(s);
+       }
+   }
