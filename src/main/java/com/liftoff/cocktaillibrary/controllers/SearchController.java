acb586643cec6fa5d113e73aedd295b4a23bb56d
AaMmbm88 <103969544+AaMmbm88@users.noreply.github.com>
2022-12-15 17:31:55 -0500

    search function first draft

--- src/main/java/com/liftoff/cocktaillibrary/controllers/SearchController.java
+++ src/main/java/com/liftoff/cocktaillibrary/controllers/SearchController.java
@@ -1,13 +1,17 @@
 package com.liftoff.cocktaillibrary.controllers;
 
+import com.liftoff.cocktaillibrary.models.Recipe;
+import com.liftoff.cocktaillibrary.models.RecipeData;
 import com.liftoff.cocktaillibrary.models.data.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping(value = "search")
+@RequestMapping("search")
 public class SearchController {
 
 
@@ -20,6 +24,18 @@ public class SearchController {
 
         return "search";
 
+    }
+
+    @PostMapping("results")
+    public String displayResults(Model model, @RequestParam String searchTerm){
+        Iterable<Recipe> recipes;
+        if (searchTerm.equals("") || searchTerm.toLowerCase().equals("all")){
+            recipes=recipeRepository.findAll();
+        }else{
+            recipes= RecipeData.findByKeyword(searchTerm, recipeRepository.findAll());
+        }
+        model.addAttribute("recipes", recipes);
+        return "search";
     }
 
 
